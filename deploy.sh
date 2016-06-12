3dc28f9888d08d60068e04fa39087d3d51d5c138
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-06-12 21:26:33 +0200

    remove methods from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,26 +7,19 @@ function build {
   npm run lint && npm run test && npm run build
 }
 
-function configGit {
-  git config user.name "Travis CI"
-  git config user.email "$COMMIT_AUTHOR_EMAIL"
-}
+# setup github deploy keys
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy.key.enc -out deploy.key -d
+chmod 600 deploy.key
+eval `ssh-agent -s`
+ssh-add deploy.key
 
-function setupDeployKeys {
-  ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-  ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-  ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-  ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-  openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy.key.enc -out deploy.key -d
-  chmod 600 deploy.key
-  eval `ssh-agent -s`
-  ssh-add deploy.key
-}
-
-function deploy {
-  wget -O deploy_to_branch.sh https://github.com/X1011/git-directory-deploy/raw/master/deploy.sh && chmod +x deploy_to_branch.sh
-  ./deploy_to_branch.sh
-}
+# config git
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
@@ -35,7 +28,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
     exit 0
 fi
 
-setupDeployKeys
-configGit
 build
-deploy
+
+wget -O deploy_to_branch.sh https://github.com/X1011/git-directory-deploy/raw/master/deploy.sh && chmod +x deploy_to_branch.sh
+./deploy_to_branch.sh
