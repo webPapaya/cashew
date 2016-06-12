c8143cb74d2063fd805332120ef4738f70af0c50
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-06-12 19:13:09 +0200

    implemented simple counter with react and rxjs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+import Rx from 'rxjs/Rx';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const MyComponent = () => <h1>First Component</h1>;
+const rerenderLoop = new Rx.Subject();
 
-ReactDOM.render(<MyComponent />, document.getElementById('main'));
+const Counter = ({ data = 0 }) => {
+  const increment = () => rerenderLoop.next(data + 1)
+  
+  return(
+    <div>
+      <div>{ data }</div>
+      <button onClick={ increment }>Click me</button>
+    </div>
+  );
+};
+
+rerenderLoop.subscribe((data) => {
+  ReactDOM.render(<Counter data={ data }/>, document.getElementById('main'));
+});
+rerenderLoop.next();
