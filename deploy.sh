6709e776d4dd3f97e95f77af3cb32c2b7bc2a147
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-06-12 18:40:44 +0200

    simplify deploy

--- deploy.sh
+++ deploy.sh
@@ -2,38 +2,40 @@
 set -e # Exit with nonzero exit code if anything fails
 
 SOURCE_BRANCH="master"
-TARGET_BRANCH="gh-pages"
 
-function doCompile {
+function build {
   npm run test && npm run build
+}
+
+function configGit {
+  git config user.name "Travis CI"
+  git config user.email "$COMMIT_AUTHOR_EMAIL"
+}
+
+function setupDeployKeys {
+  ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+  ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+  ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+  ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+  openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy.key.enc -out deploy.key -d
+  chmod 600 deploy.key
+  eval `ssh-agent -s`
+  ssh-add deploy.key
+}
+
+function deploy {
+  wget -o deploy_to_branch.sh https://github.com/X1011/git-directory-deploy/raw/master/deploy.sh && chmod +x deploy_to_branch.sh
+  ./deploy_to_branch.sh
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
-    doCompile
+    build
     exit 0
 fi
 
-# Save some useful information
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy.key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy.key.enc -out deploy.key -d
-chmod 600 deploy.key
-eval `ssh-agent -s`
-ssh-add deploy.key
-
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-
-
-doCompile
-wget -o deploy_to_branch.sh https://github.com/X1011/git-directory-deploy/raw/master/deploy.sh && chmod +x deploy_to_branch.sh
-./deploy_to_branch.sh
+setupDeployKeys
+configGit
+build
+deploy
