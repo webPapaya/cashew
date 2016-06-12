ebbeea578c438b360de59ad63f97070a2229255e
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-06-12 23:23:01 +0200

    extract

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import { createStore } from './store';
 import { createActions } from './actions';
 
+var INITIAL_STORE_DATA = { counter: 0 };
 
-const store = createStore({ counter: 0 });
+const store = createStore(INITIAL_STORE_DATA);
 const actions = createActions({ store });
 
 const Counter = ({ clickAmount }) =>
@@ -12,8 +13,6 @@ const Counter = ({ clickAmount }) =>
     <div>{ clickAmount }</div>
     <button onClick={ actions.incrementCounter }>Click me</button>
   </div>;
-
-
 
 store.subscribe(() => {
   const { counter } = store.read();
