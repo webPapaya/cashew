43f4863c0413b82b2bc61ef6a7899622b833f368
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-06-12 17:01:22 +0200

    updated deploy script so that only the dist directory is being deployed to gh-pages

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  npm run test && npm build
+  npm run test && npm run build
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
@@ -20,35 +20,6 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into dist/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO dist
-cd dist
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
-
-# Clean dist existing contents
-rm -rf dist/**/* || exit 0
-
-# Run our compile script
-doCompile
-
-# Now let's go have some fun with the cloned repo
-cd dist
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
-
-# Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
-git add .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
-
 # Get the deploy key by using Travis's stored variables to decrypt deploy.key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
@@ -59,5 +30,32 @@ chmod 600 deploy.key
 eval `ssh-agent -s`
 ssh-add deploy.key
 
-# Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+# Clone the existing gh-pages for this repo into dist/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+git clone $REPO dist
+cd dist
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
+
+# Clean dist existing contents
+rm -rf dist/**/* || exit 0
+
+
+# Now let's go have some fun with the cloned repo
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# Run our compile script
+mkdir -p dist
+git subtree pull --prefix dist origin $TARGET_BRANCH
+doCompile
+
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if [ -z `git diff --exit-code` ]; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
+git add dist -f
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+git subtree push --prefix dist origin $TARGET_BRANCH
