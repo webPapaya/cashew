3938bb48151b8d28149e5a2eff3e321207733f66
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-06-15 18:13:24 +0200

    combine test suite because offline and session storage should behave exactly the same

---
+++ src/storage/storage.spec.js
@@ -0,0 +1,45 @@
+import {
+  assertThat,
+  equalTo,
+} from 'hamjest';
+
+import { createOfflineStorage } from './offline-storage';
+import { createSessionStorage } from './session-storage';
+
+[
+  { name: 'session', createStorage: createSessionStorage},
+  { name: 'offline', createStorage: createOfflineStorage}
+].forEach(({ name, createStorage }) => {
+  describe(`${name} storage`, () => {
+    describe('retrieve', () => {
+      it('responds the whole store', () => {
+        const data = { dummy: 'data' };
+        const offlineStore = createStorage({ initialData: data });
+
+        assertThat(offlineStore.retrieve(), equalTo(data));
+      });
+    });
+
+    describe('update', () => {
+      it('updates store by given object', () => {
+        const offlineStore = createStorage();
+
+        const data = { myKey: 'myValue' };
+        offlineStore.update(data);
+        assertThat(offlineStore.retrieve(), equalTo(data));
+      });
+
+      it('AND doesn\'t override existing data', () => {
+        const existingData = { existing: 'value' };
+        const updateData = { myKey: 'myValue' };
+
+        const offlineStore = createStorage({ initialData: existingData });
+        offlineStore.update(updateData);
+
+        assertThat(offlineStore.retrieve(),
+          equalTo({ ...existingData, ...updateData }));
+      });
+    });
+  });
+});
+
