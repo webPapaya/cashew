9891cb26c534c34f43c64df574acae0e79b1b85b
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-06-15 19:18:07 +0200

    cleanup

--- src/store.js
+++ src/store.js
@@ -4,25 +4,23 @@ import {
 } from './storage';
 
 export const createStore = (initialData = {}) => {
-  let data = initialData;
   const updateCallbacks = [];
   const offlineStorage = createOfflineStorage();
   const sessionStorage = createSessionStorage();
 
   const retrieve = () => ({
-    ...data,
     ...offlineStorage.retrieve(),
     ...sessionStorage.retrieve(),
   });
 
   const subscribe = (next) => {
-    next(data);
+    next(retrieve());
     updateCallbacks.push(next);
   };
 
   const notify = () => {
     updateCallbacks
-      .forEach((callback) => { callback(data); });
+      .forEach((callback) => { callback(retrieve()); });
   };
 
   const saveOffline = (newData = {}) => {
