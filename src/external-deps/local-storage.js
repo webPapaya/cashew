91d6928ec0380c9daaf7ac89863d4f352ea235f0
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-06-15 08:22:25 +0200

    implement browser and node local storage

--- src/external-deps/local-storage.js
+++ src/external-deps/local-storage.js
@@ -4,25 +4,35 @@ import {
   getEnv,
 } from '../environments';
 
+const nodeLocalStorage = (initialData = {}) => {
+  let storedData = initialData;
+  const retrieveStorage = () =>
+    JSON.stringify(storedData);
+
+  const updateStorage = (updateData) =>
+    storedData = JSON.parse(updateData);
+
+  return { retrieveStorage, updateStorage };
+};
+
+const browserLocalStorage = (initialData = JSON.stringify({})) => {
+  const STORAGE_KEY = 'CASHEW_STORAGE';
+
+  const retrieveStorage = () =>
+    window.localStorage.getItem(STORAGE_KEY);
+
+  const updateStorage = (updateData) =>
+    window.location.setItem(STORAGE_KEY, updateData);
+
+  updateStorage(initialData);
+
+  return { retrieveStorage, updateStorage };
+};
+
 export const createLocalStorageAdapter = (data = {}) => {
   if(getEnv() === TESTING) {
-    let storedData = data;
-    const retrieveStorage = () =>
-      JSON.stringify(storedData);
-
-    const updateStorage = (updateData) =>
-      storedData = JSON.parse(updateData);
-
-    return { retrieveStorage, updateStorage };
+    return nodeLocalStorage(data);
   } else if (getEnv() === DEFAULT) {
-    const STORAGE_KEY = 'CASHEW_STORAGE';
-
-    const retrieveStorage = () =>
-      window.localStorage.getItem(STORAGE_KEY);
-
-    const updateStorage = (updateData) =>
-      window.location.setItem(STORAGE_KEY, updateData);
-
-    return { retrieveStorage, updateStorage };
+    return browserLocalStorage(data)
   }
 };
