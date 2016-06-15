ad972913c14224d5138270a50e6a9de76b038007
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-06-15 18:15:51 +0200

    move storage out of direcoty again

--- src/storage.js
+++ src/storage.js
@@ -1,4 +1,4 @@
-import { createStorageAdapter } from '../external-deps/local-storage';
+import { createStorageAdapter } from './external-deps/local-storage';
 
 export const createOfflineStorage = ({ initialData = {}, adapterFn = createStorageAdapter } = {}) => {
   const adapter = adapterFn(initialData);
