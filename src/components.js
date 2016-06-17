444f0175178685c1598ff0dc251694c1ab5d9b4e
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-06-18 01:06:39 +0200

    move create components into components file

--- src/components.js
+++ src/components.js
@@ -24,6 +24,37 @@ const Timer = ({ currentTime }) => {
   );
 };
 
+
+export const createComponents = (componentsDefinition) => {
+  return componentsDefinition.map((componentDefinition) => {
+    let isInitialized = false;
+
+    const initialize = (...args) => {
+      if (componentDefinition.initialize) {
+        componentDefinition.initialize(...args);
+      }
+
+      isInitialized = true;
+    };
+
+    const destruct = (...args) => {
+      if (componentDefinition.destruct) {
+        componentDefinition.destruct(...args);
+      }
+
+      isInitialized = false;
+    };
+
+    return {
+      ...componentDefinition,
+      initialize,
+      destruct,
+      isInitialized() { return isInitialized; }
+    };
+  });
+};
+
+
 const COMPONENTS = [
   {
     domId: 'counter-1',
@@ -62,7 +93,3 @@ export const components = COMPONENTS.map((component) => {
     ...component,
   };
 });
-
-export const createComponents = () => {
-  
-};
--- src/components.spec.js
+++ src/components.spec.js
@@ -3,34 +3,7 @@ import {
   equalTo,
 } from 'hamjest';
 
-const createComponents = (componentsDefinition) => {
-  return componentsDefinition.map((componentDefinition) => {
-    let isInitialized = false;
-
-    const initialize = (...args) => {
-      if (componentDefinition.initialize) {
-        componentDefinition.initialize(...args);
-      }
-
-      isInitialized = true;
-    };
-
-    const destruct = (...args) => {
-      if (componentDefinition.destruct) {
-        componentDefinition.destruct(...args);
-      }
-
-      isInitialized = false;
-    };
-
-    return {
-      ...componentDefinition,
-      initialize,
-      destruct,
-      isInitialized() { return isInitialized; }
-    };
-  });
-};
+import { createComponents } from './components';
 
 describe('createComponent', () => {
   it('given properties are bypassed', () => {
