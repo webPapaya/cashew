8edffcf58675afd9766cb53e6a38773b2997bb76
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-06-18 01:07:54 +0200

    cleanup

--- src/components.js
+++ src/components.js
@@ -1,30 +1,5 @@
 import React from 'react';
 
-const Counter = ({ counts, actions }) => {
-  const updateCounter = (evt) => {
-    const inputValue = parseInt(evt.currentTarget.value, 10);
-    actions.updateCounter(inputValue);
-  };
-
-  return (
-    <div>
-      <div>{ counts }</div>
-      <button onClick={ actions.incrementCounter }>Click me</button>
-      <input type="range" min="-100" max="100" value={ counts } onChange={ updateCounter }/>
-    </div>
-  );
-};
-
-const Timer = ({ currentTime }) => {
-  if (!currentTime) { return <div></div>; }
-  return (
-    <div>
-      { currentTime.toUTCString() }
-    </div>
-  );
-};
-
-
 export const createComponents = (componentsDefinition) => {
   return componentsDefinition.map((componentDefinition) => {
     let isInitialized = false;
@@ -55,10 +30,36 @@ export const createComponents = (componentsDefinition) => {
 };
 
 
+
+const Counter = ({ counts, actions }) => {
+  const updateCounter = (evt) => {
+    const inputValue = parseInt(evt.currentTarget.value, 10);
+    actions.updateCounter(inputValue);
+  };
+
+  return (
+    <div>
+      <div>{ counts }</div>
+      <button onClick={ actions.incrementCounter }>Click me</button>
+      <input type="range" min="-100" max="100" value={ counts } onChange={ updateCounter }/>
+    </div>
+  );
+};
+
+const Timer = ({ currentTime }) => {
+  if (!currentTime) { return <div></div>; }
+  return (
+    <div>
+      { currentTime.toUTCString() }
+    </div>
+  );
+};
+
+
+
 const COMPONENTS = [
   {
     domId: 'counter-1',
-    initialized: false,
     renderComponent: ({ appState, actions }) => {
       const { counts } = appState;
       return <Counter counts={ counts } actions={ actions }/>;
@@ -69,15 +70,12 @@ const COMPONENTS = [
     initialize: ({ actions }) => {
       actions.startClock();
     },
-    initialized: false,
     renderComponent({ appState }) {
       const { currentTime } = appState;
       return <Timer currentTime={ currentTime } />;
     },
   }, {
     domId: 'counter-3',
-    initialize() {},
-    initialized: false,
     renderComponent: ({ appState, actions }) => {
       const { counts } = appState;
       return <Counter counts={ counts } actions={ actions } />;
@@ -85,11 +83,4 @@ const COMPONENTS = [
   },
 ];
 
-export const components = COMPONENTS.map((component) => {
-  return {
-    initialize: () => {},
-    destruct: () => {},
-    initialized: false,
-    ...component,
-  };
-});
+export const components = createComponents(COMPONENTS);
