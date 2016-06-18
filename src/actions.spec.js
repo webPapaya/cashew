29b719eac59fee0020c07eb68dcb5c7b72725183
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-06-18 10:31:23 +0200

    remove dummy components and actions

--- src/actions.spec.js
+++ src/actions.spec.js
@@ -3,29 +3,9 @@ import {
   equalTo,
 } from 'hamjest';
 
-import { createStore } from './lib/store';
-import { createActions } from './actions';
-
-describe('Actions', () => {
-  describe('#updateCounter', () => {
-    it('saves the new count', () => {
-      const store = createStore();
-      const actions = createActions({ store });
-
-      actions.updateCounter(5);
-
-      assertThat(store.retrieve(), equalTo({ counts: 5 }));
-    });
-  });
-
-  describe('#incrementCounter', () => {
-    it('increments the counter by 1', () => {
-      const store = createStore();
-      const actions = createActions({ store });
-
-      actions.incrementCounter();
-
-      assertThat(store.retrieve(), equalTo({ counts: 1 }));
-    });
-  });
-});
+// import { createStore } from './lib/store';
+// import { createActions } from './actions';
+//
+// describe('Actions', () => {
+//
+// });
--- src/components.js
+++ src/components.js
@@ -1,58 +1,15 @@
 import React from 'react';
 import { createComponents } from './lib/components';
 
-const Counter = ({ counts, actions }) => {
-  const updateCounter = (evt) => {
-    const inputValue = parseInt(evt.currentTarget.value, 10);
-    actions.updateCounter(inputValue);
-  };
-
-  return (
-    <div>
-      <div>{ counts }</div>
-      <button onClick={ actions.incrementCounter }>Click me</button>
-      <input type="range" min="-100" max="100" value={ counts } onChange={ updateCounter }/>
-    </div>
-  );
-};
-
-const Timer = ({ currentTime }) => {
-  if (!currentTime) { return <div></div>; }
-  return (
-    <div>
-      { currentTime.toUTCString() }
-    </div>
-  );
-};
-
-const ClockControlls = ({ actions }) => {
-  return (
-    <div>
-      <button onClick={ actions.addClock }>Add Clock</button>
-      <button onClick={ actions.removeClock }>Remove Clock</button>
-    </div>
-  );
-};
+const DirectoryListing = ({ actions }) => {};
 
 const COMPONENTS = [
   {
     domId: 'counter-1',
-    render({ appState, actions }) {
-      const { counts } = appState;
-      return <Counter counts={ counts } actions={ actions }/>;
-    },
-  }, {
-    domId: 'counter-2',
-    render({ appState }) {
-      const { currentTime } = appState;
-      return <Timer currentTime={ currentTime } />;
-    },
-  }, {
-    domId: 'counter-3',
     render({ actions }) {
-      return <ClockControlls actions={ actions } />;
+      return <DirectoryListing actions={ actions }/>;
     },
-  },
+  }
 ];
 
 export const components = createComponents(COMPONENTS);
