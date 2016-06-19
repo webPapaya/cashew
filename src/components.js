4e6caa7a23b03f7d7ed2129380a499f154d7ee1e
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-06-19 19:45:21 +0200

    show loading

--- src/components.js
+++ src/components.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createComponents } from './lib/components';
 
-const File = ({ name, loaded, key }) => {
+const File = ({ name, loaded, key, imgSrc }) => {
   const loadStatus = loaded
     ? "was loaded"
     : "not loaded yet";
@@ -9,21 +9,30 @@ const File = ({ name, loaded, key }) => {
   return (
     <li key={ key }>
       { name }, { loadStatus }
+      <img src={ imgSrc } alt={ name } />
     </li>
   );
 };
 
-const DirectoryListing = ({ actions, fileList }) => {
+const DirectoryListing = ({ actions, fileList, allFilesLoaded }) => {
   const readFiles = (evt) => {
     actions.readFiles(evt.target.files);
   };
+
+  if(!allFilesLoaded) {
+    return (
+      <div>
+        loading files
+      </div>
+    )
+  }
 
   return (
     <div>
       <input type="file" multiple onChange={ readFiles }/>
       <ul>
-        { fileList.map(({name, loaded}, index) =>
-          <File name={ name } loaded={ loaded } key={ index } />) }
+        { fileList.map(({name, loaded, dataUrl }, index) =>
+          <File name={ name } loaded={ loaded } imgSrc={ dataUrl } key={ index } />) }
       </ul>
     </div>
   );
@@ -34,7 +43,8 @@ const COMPONENTS = [
     domId: 'counter-1',
     render({ actions, appState }) {
       const { fileList = [] } = appState;
-      return <DirectoryListing actions={ actions } fileList={ fileList } />;
+      const allFilesLoaded = fileList.every((file) => file.loaded );
+      return <DirectoryListing actions={ actions } fileList={ fileList } allFilesLoaded={ allFilesLoaded } />;
     },
   }
 ];
