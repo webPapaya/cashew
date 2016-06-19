b12e0a1b1eb9607680552ee605846a113b1e1df9
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-06-19 22:23:48 +0200

    update broken local storage

--- src/lib/store/index.js
+++ src/lib/store/index.js
@@ -10,6 +10,7 @@ export const createStore = ({ sessionData = {}, offlineData = {}, locationData =
   const offlineStorage = createOfflineStorage({ initialData: offlineData });
   const sessionStorage = createSessionStorage({ initialData: sessionData });
   const locationStorage = createLocationStorage({ initialData: locationData });
+  localStorage.update({});
 
   const retrieve = () => {
     const localStorageAndOffline = merge(locationStorage.retrieve(), offlineStorage.retrieve());
