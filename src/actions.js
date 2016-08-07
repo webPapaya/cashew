f6e94afc5a0be3a20cc5b2db24ede28513ce76d4
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-08-07 21:08:51 +0200

    Extracted timer actions into own directory.

--- src/actions.js
+++ src/actions.js
@@ -1,41 +1,4 @@
-const createTimerActions = ({ store }) => {
-  const tick = () => {
-    const { duration = 0 } = store.retrieve();
-    store.saveInSession({ duration: duration + 1 });
-  };
-
-  const pause = () => {
-    const { interval } = store.retrieve();
-    global.clearInterval(interval);
-  };
-
-  const start = () => {
-    stopInterval();
-    startInterval();
-  };
-
-  const stop = () => {
-    stopInterval();
-    resetDuration();
-  };
-
-  const resetDuration = () => {
-    store.saveInSession({ duration: 0 });
-  };
-
-  const stopInterval = () => {
-    const { interval } = store.retrieve();
-    global.clearInterval(interval);
-  };
-
-  const startInterval = () => {
-    const interval = global.setInterval(() => { tick() }, 1000);
-    store.saveInSession({ interval });
-  };
-
-  return { tick, pause, start, stop };
-};
-
+import { createActions as createTimerActions } from './screens/timer/index';
 
 export const createActions = ({ store }) => {
   const timerActions = createTimerActions({ store });
