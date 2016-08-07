9ab410ac5969f7168ee25f9e5d1780bf6a74136a
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-08-07 19:54:44 +0200

    Remove file reader and added a basic timer.

--- src/actions.js
+++ src/actions.js
@@ -1,34 +1,8 @@
-const filesToFileList = (files) => {
-  const fileList = [];
-  for (let i = 0; i < files.length; i++) {
-    const file = files[i];
-    fileList.push(file);
-  }
-  return fileList;
-};
-
 export const createActions = ({ store }) => {
-  const readFile = (file, id) => {
-    const reader = new FileReader();
-    const { fileList } = store.retrieve();
-
-    reader.onload = ({ target }) => {
-      const dataUrl = target.result;
-
-      fileList[id].dataUrl = dataUrl;
-      fileList[id].loaded = true;
-
-      store.saveInSession({ fileList });
-    };
-
-    reader.readAsDataURL(file);
+  const tick = () => {
+    const { duration } = store.retrieve();
+    store.saveInSession({ duration: duration + 1 });
   };
-
-  const readFiles = (files) => {
-    const fileList = filesToFileList(files);
-    store.saveInSession({ fileList });
-    fileList.forEach(readFile);
-  };
-
-  return { readFiles };
+  
+  return { tick };
 };
--- src/components.js
+++ src/components.js
@@ -1,60 +1,25 @@
 import React from 'react';
 import { createComponents } from './lib/components';
 
-const Loading = ({ allFiles, loadedFiles }) =>
-  <div>
-    { loadedFiles.length } of { allFiles.length } already loaded
-  </div>
-;
 
-const Files = ({ actions, fileList }) => {
-  const readFiles = (evt) =>
-    actions.readFiles(evt.target.files);
-
-  return (
-    <div>
-      <input type="file" multiple onChange={ readFiles }/>
-      <ul>
-        { fileList.map(({ name, loaded, dataUrl }, index) =>
-          <File name={ name } loaded={ loaded } imgSrc={ dataUrl } key={ index } />) }
-      </ul>
-    </div>
-  );
-};
-
-const File = ({ name, loaded, key, imgSrc }) => {
-  const loadStatus = loaded
-    ? 'was loaded'
-    : 'not loaded yet';
-
-  return (
-    <li key={ key }>
-      { name }, { loadStatus }
-      <img src={ imgSrc } alt={ name } />
-    </li>
-  );
-};
-
-const DirectoryListing = ({ actions, fileList, loadedFiles }) => {
-  const isFullyLoaded = loadedFiles.length !== fileList.length;
-  return isFullyLoaded
-    ? <Loading allFiles={ fileList } loadedFiles={ loadedFiles } />
-    : <Files actions={ actions } fileList={ fileList } />;
+const Timer = ({ duration }) => {
+  return <div>{ duration }</div>
 };
 
 const COMPONENTS = [
   {
     domId: 'counter-1',
-    render({ actions, appState }) {
-      const { fileList = [] } = appState;
-      const loadedFiles = fileList.filter((file) => file.loaded);
-      return (
-        <DirectoryListing
-          actions={ actions }
-          fileList={ fileList }
-          loadedFiles={ loadedFiles }
-        />
-      );
+    construct({ store, actions }) {
+      const interval = global.setInterval(() => { actions.tick() }, 1000);
+      store.saveInSession({ interval, duration: 0 });
+    },
+    render({ appState }) {
+      const { duration } = appState;
+      return <Timer duration={ duration } />
+    },
+    destruct({ store }) {
+      const { interval } = store.retrieve();
+      global.clearInterval(interval);
     },
   },
 ];
