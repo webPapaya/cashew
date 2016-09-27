935767d7160643a4a87dae5cf183aedd333c656d
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-09-27 22:47:33 +0200

    Added basic flow.

--- src/actions.js
+++ src/actions.js
@@ -2,8 +2,6 @@ import { createActions as createTimerActions } from './screens/timer/index';
 import { createActions as createGithubProfileActions } from './screens/github-profile/index';
 
 export const createActions = ({ store }) => {
-  const timer = createTimerActions({ store });
   const githubProfile = createGithubProfileActions({ store });
-
-  return { timer: timer, githubProfile: githubProfile };
+  return { ...githubProfile };
 };
--- src/screens/github-profile/index.js
+++ src/screens/github-profile/index.js
@@ -1,25 +1,74 @@
 import React from 'react';
 
-export const createActions = ({ store }) => {
-  const initialize = () => {
-    store.saveInSession({ initializing: true });
+const FORBIDDEN = 400;
+const apiSignIn = ({ username, password }) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      store.saveInSession({ initializing: false });
-    }, 100);
+      if( username === 'username' && password === 'password' ) {
+        return resolve({ username , password });
+      }
+      reject({ statusCode: FORBIDDEN });
+    }, 500);
+  });
+};
+
+export const createActions = ({ store }) => {
+  const showLoadingScreen = () => {
+    store.saveInSession({ currentScreen: 'loading' });
   };
 
-  return { initialize };
+  const showSignInScreen = () => {
+    store.saveInSession({ currentScreen: 'sign-in' });
+  };
+
+  const showApplicationScreen = () => {
+    store.saveInSession({ currentScreen: 'application' });
+  };
+
+  const signIn = ({ username, password }) => {
+    Promise.resolve()
+      .then(showLoadingScreen)
+      .then(() => apiSignIn({ username, password }))
+      .then(showApplicationScreen)
+      .catch(showSignInScreen);
+  };
+
+  return { showLoadingScreen, signIn };
+};
+
+
+
+
+
+class SignInScreen extends React.Component {
+  render() {
+    const onSubmit = () => {
+      const username = this.refs.username.value;
+      const password = this.refs.password.value;
+      this.props.onSubmit({ username, password });
+    };
+
+    return (
+      <form>
+        <input ref="username" type="text" />
+        <input ref="password" type="password" />
+        <button onClick={ onSubmit }>Login</button>
+      </form>
+    );
+  }
+}
+
+const SCREENS = {
+  loading: () => <div>Loading</div>,
+  application: () => <div>Application</div>,
+  default: ({ actions }) => <SignInScreen onSubmit={ actions.signIn } />,
 };
 
 export const COMPONENTS = [{
   domId: 'counter-1',
-  construct({ actions }) {
-    actions.githubProfile.initialize();
-  },
-  render({ appState }) {
-    const { initializing } = appState;
-    return (
-      <div>{ initializing ? 'loading' : 'not loading' }</div>
-    );
+  render({ appState, actions }) {
+    const { currentScreen } = appState;
+    const screenToRender = SCREENS[currentScreen] || SCREENS.default;
+    return screenToRender({ appState, actions });
   },
 }];
