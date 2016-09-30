b9b136298565d85c77e1d9ce7a2042c2e162f863
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-09-30 17:53:29 +0200

    Extracted create component.

--- handcrafted/src/lib/components.js
+++ handcrafted/src/lib/components.js
@@ -1,27 +1,29 @@
+export const createComponent = (componentDefinition) => {
+  let isInitialized = false;
+
+  const construct = (...args) => {
+    if (!isInitialized && componentDefinition.construct) {
+      componentDefinition.construct(...args);
+    }
+
+    isInitialized = true;
+  };
+
+  const destruct = (...args) => {
+    if (isInitialized && componentDefinition.destruct) {
+      componentDefinition.destruct(...args);
+    }
+
+    isInitialized = false;
+  };
+
+  return {
+    ...componentDefinition,
+    construct,
+    destruct,
+    isInitialized() { return isInitialized; },
+  };
+};
+
 export const createComponents = (componentsDefinition) =>
-  componentsDefinition.map((componentDefinition) => {
-    let isInitialized = false;
-
-    const construct = (...args) => {
-      if (!isInitialized && componentDefinition.construct) {
-        componentDefinition.construct(...args);
-      }
-
-      isInitialized = true;
-    };
-
-    const destruct = (...args) => {
-      if (isInitialized && componentDefinition.destruct) {
-        componentDefinition.destruct(...args);
-      }
-
-      isInitialized = false;
-    };
-
-    return {
-      ...componentDefinition,
-      construct,
-      destruct,
-      isInitialized() { return isInitialized; },
-    };
-  });
+  componentsDefinition.map(createComponent);
