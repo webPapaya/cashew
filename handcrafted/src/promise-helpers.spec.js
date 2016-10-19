f5a87eee6c7e08acfc92b35f71b029b878252962
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-10-19 19:10:05 +0200

    Rethrow errors.

--- handcrafted/src/promise-helpers.spec.js
+++ handcrafted/src/promise-helpers.spec.js
@@ -5,12 +5,25 @@ const ignoreReturnFor = (fn) => (arg) => {
   return arg;
 };
 
+const rethrowError = (fn) => (error) => Promise.resolve()
+  .then(() => fn(error))
+  .then(() => { throw error });
+
 describe('ignoreReturnFor', () => {
   it('ignores return value', () => Promise.resolve()
     .then(() => '1 value')
     .then(ignoreReturnFor(() => '2 value'))
     .then((value) =>
       assertThat(value, equalTo('1 value'))));
+});
+
+describe('rethrowError', () => {
+  it('ignores return value', () => Promise.resolve()
+    .then(() => { throw 'my error'; })
+    .catch(rethrowError((error) => error))
+    .then(() => assertThat(false, equalTo(true)))
+    .catch((error) =>
+      assertThat(error, equalTo('my error'))));
 });
 
 
